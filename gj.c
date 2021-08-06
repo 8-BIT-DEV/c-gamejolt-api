34e60e803fa86a86087b81c8bf53f0b2721e39cf
8-BIT-DEV <44956905+8-BIT-DEV@users.noreply.github.com>
2021-08-05 22:59:00 -0400

    Put the V2 files on the repo

--- gj.c
+++ gj.c
@@ -3,7 +3,6 @@
 #include <openssl/sha.h>
 #include <string.h>
 #include <http-get.h>
-#include <jsmn.h>
 
 #include <stdarg.h>
 
@@ -12,6 +11,11 @@ char* pkey;
 
 char username[96];
 char token[96];
+
+int errcode = 0;
+char errorinfo[128];
+
+char* gj_opstrings[] = {"append", "prepend", "add", "subtract", "multiply", "divide"};
 
 char* p_gethash(char* in)
 {
@@ -37,6 +41,7 @@ char* p_makeurl(char* endpoint, int argcount, ...)
     strcpy(url, "https://api.gamejolt.com/api/game/v1_2/");
     strcat(url, endpoint);
     strcat(url, "/?");
+    strcat(url, "format=keypair&");
     for(int i = 0; i < argcount; i++)
     {
         char* name = va_arg(args, char*);
@@ -58,32 +63,98 @@ char* p_makeurl(char* endpoint, int argcount, ...)
     return strdup(url);
 }
 
-int p_checkgood(http_get_response_t* got)
+gj_keydata_t p_checkgood(http_get_response_t* got)
 {
-    jsmn_parser p;
-    jsmntok_t tokens[128];
-    jsmn_init(&p);
-    jsmn_parse(&p, got->data, got->size, tokens, 128);
-    //printf("TOKN:%u\n", p.toknext);
-    //printf("DATA:%s\n", got->data);
-    for(int i = 0; i < p.toknext; i++)
+    gj_keydata_t data = p_parse_reponse(got->data);
+    int sid = p_data_get_index(data, "success");
+
+    if(sid == -1) {printf("BAD REQUEST : %s\n", got->data); data.status = 0;}
+    else if(sid != -1)
     {
-        if(strncmp(&got->data[tokens[i].start], "success", tokens[i].end - tokens[i].start) == 0)
+        if(strcmp(data.values[sid], "false") == 0)
         {
-            if(strncmp(&got->data[tokens[i+1].start], "true", tokens[i+1].end - tokens[i+1].start) == 0) return 1;
-            else
-            {
-                printf("INFO:%s\n", got->data);
-                return 0;
-            }
+            data.status = 0;
+            printf("ERROR IN API\n");
         }
     }
-    return 0;
+
+    errcode = data.status;
+    return data;
+}
+
+gj_keydata_t p_parse_reponse(char* repo)
+{
+    gj_keydata_t data;
+    data.count = 0;
+    data.status = 1;
+    int state = 0;
+    int off = 0;
+
+    memset(data.names, 0, 64 * 64);
+    memset(data.values, 0, 64 * 64);
+
+    for(int i = 0; i < strlen(repo); i++)
+    {
+        if(state == 0)
+        {
+            if(repo[i] != ':')
+            {
+                data.names[data.count][off] = repo[i];
+                off++;
+            } else if(repo[i] == ':')
+            {
+                data.names[data.count][off] = '\0';
+                state = 1;
+                off = 0;
+            }
+        } else if(state == 1)
+        {
+            if(repo[i] != '\n' && repo[i] != '\0')
+            {
+                if(repo[i] != '\"')
+                {
+                    data.values[data.count][off] = repo[i];
+                    off++;
+                }
+            } else if(repo[i] == '\n' || repo[i] == '\0')
+            {
+                data.values[data.count][off-1] = '\0';
+                state = 2;
+                off = 0;
+            }
+        }
+
+        if(state == 2)
+        {
+            off = 0;
+            state = 0;
+            data.count++;
+        }
+    }
+
+    data.count++;
+    return data;
+}
+
+int p_data_get_index(gj_keydata_t data, const char* name)
+{
+    for(int i = 0; i < data.count; i++)
+    {
+        if(strcmp(data.names[i], name) == 0) return i;
+    }
+    return -1;
 }
 
 void gj_api()
 {
     // added this and never used it :)
+}
+
+int gj_error()
+{
+    int perrcode = errcode;
+    errcode = 0;
+    return perrcode;
 }
 
 int gj_login(const char* ply_name, const char* ply_token)
@@ -92,34 +163,101 @@ int gj_login(const char* ply_name, const char* ply_token)
     strcpy(token, ply_token);
     char* url = p_makeurl("users/auth", 3, "game_id", id, "username", username, "user_token", token);
     http_get_response_t* got = http_get(url);
-    int code = p_checkgood(got);
+    gj_keydata_t code = p_checkgood(got);
     http_get_free(got);
     free_url(url);
-    return code;
+    return code.status;
 }
 
-int gj_give_trophie(int tid)
+gj_userdata_t gj_get_user(const char* username, int uid)
+{
+    char uids[32];
+    sprintf(uids, "%d", uid);
+    char* url = p_makeurl("users/fetch", 3, "game_id", id, "username", username, "user_id", uids);
+    http_get_response_t* got = http_get(url);
+    gj_keydata_t code = p_checkgood(got);
+    http_get_free(got);
+    free_url(url);
+
+    gj_userdata_t user;
+    user.status[0] = NULL;
+
+    if(code.status == 0) return user;
+
+    user.id = atoi(code.values[1]);
+    strcpy(user.type, code.values[2]);
+    strcpy(user.username, code.values[3]);
+    strcpy(user.avatar, code.values[4]);
+    strcpy(user.signed_up, code.values[5]);
+    user.signed_up_timestamp = atoi(code.values[6]);
+    strcpy(user.last_logged_in, code.values[7]);
+    user.last_logged_in_timestamp = atoi(code.values[8]);
+    strcpy(user.status, code.values[9]);
+    strcpy(user.dev_name, code.values[10]);
+    strcpy(user.dev_website, code.values[11]);
+    strcpy(user.dev_desc, code.values[12]);
+
+    return user;
+}
+
+void gj_free_user(gj_userdata_t user)
+{
+    free(user.type);
+    free(user.username);
+    free(user.avatar);
+    free(user.signed_up);
+    free(user.last_logged_in);
+    free(user.dev_name);
+    free(user.dev_website);
+    free(user.dev_desc);
+}
+
+gj_trophy_t gj_get_trophy(int tid)
+{
+    char stid[32];
+    sprintf(stid, "%d", tid);
+    char* url = p_makeurl("trophies", 4, "game_id", id, "username", username, "user_token", token, "trophy_id", stid);
+    http_get_response_t* got = http_get(url);
+    gj_keydata_t code = p_checkgood(got);
+    http_get_free(got);
+    free_url(url);
+
+    gj_trophy_t tdata;
+
+    if(code.status == 0) return tdata;
+
+    tdata.id = atoi(code.values[1]);
+    strcpy(tdata.title, code.values[2]);
+    strcpy(tdata.diff, code.values[3]);
+    strcpy(tdata.desc, code.values[4]);
+    strcpy(tdata.image, code.values[5]);
+    strcpy(tdata.given, code.values[6]);
+
+    return tdata;
+}
+
+int gj_give_trophy(int tid)
 {
     char stid[32];
     sprintf(stid, "%d", tid);
     char* url = p_makeurl("trophies/add-achieved", 4, "game_id", id, "username", username, "user_token", token, "trophy_id", stid);
     http_get_response_t* got = http_get(url);
-    int code = p_checkgood(got);
+    gj_keydata_t code = p_checkgood(got);
     http_get_free(got);
     free_url(url);
-    return code;
+    return code.status;
 }
 
-int gj_revoke_trophie(int tid)
+int gj_revoke_trophy(int tid)
 {
     char stid[32];
     sprintf(stid, "%d", tid);
     char* url = p_makeurl("trophies/remove-achieved", 4, "game_id", id, "username", username, "user_token", token, "trophy_id", stid);
     http_get_response_t* got = http_get(url);
-    int code = p_checkgood(got);
+    gj_keydata_t code = p_checkgood(got);
     http_get_free(got);
     free_url(url);
-    return code;
+    return code.status;
 }
 
 int gj_add_score(int tid, int score)
@@ -130,10 +268,24 @@ int gj_add_score(int tid, int score)
     sprintf(sscore, "%d", score);
     char* url = p_makeurl("scores/add", 6, "game_id", id, "username", username, "user_token", token, "score", sscore, "sort", sscore, "table_id", stid);
     http_get_response_t* got = http_get(url);
-    int code = p_checkgood(got);
+    gj_keydata_t code = p_checkgood(got);
     http_get_free(got);
     free_url(url);
-    return code;
+    return code.status;
+}
+
+int gj_add_score_extra(int tid, int score, const char* data)
+{
+    char stid[32];
+    char sscore[32];
+    sprintf(stid, "%d", tid);
+    sprintf(sscore, "%d", score);
+    char* url = p_makeurl("scores/add", 7, "game_id", id, "username", username, "user_token", token, "score", sscore, "sort", sscore, "table_id", stid, "extra_data", data);
+    http_get_response_t* got = http_get(url);
+    gj_keydata_t code = p_checkgood(got);
+    http_get_free(got);
+    free_url(url);
+    return code.status;
 }
 
 int gj_add_guest_score(const char* name, int tid, int score)
@@ -144,44 +296,209 @@ int gj_add_guest_score(const char* name, int tid, int score)
     sprintf(sscore, "%d", score);
     char* url = p_makeurl("scores/add", 5, "game_id", id, "guest", name, "score", sscore, "sort", sscore, "table_id", stid);
     http_get_response_t* got = http_get(url);
-    int code = p_checkgood(got);
+    gj_keydata_t code = p_checkgood(got);
     http_get_free(got);
     free_url(url);
-    return code;
+    return code.status;
+}
+
+gj_rank_t gj_get_rank(int tid, int score)
+{
+    char stid[32];
+    char sscore[32];
+    sprintf(stid, "%d", tid);
+    sprintf(sscore, "%d", score);
+    char* url = p_makeurl("scores/get-rank", 3, "game_id", id, "table_id", stid, "sort", sscore);
+    http_get_response_t* got = http_get(url);
+    gj_keydata_t code = p_checkgood(got);
+    http_get_free(got);
+    free_url(url);
+
+
+    gj_rank_t rank;
+    if(code.status == 0) return rank;
+
+    rank = atoi(code.values[1]);
+
+    return rank;
 }
 
 int gj_open_session()
 {
     char* url = p_makeurl("sessions/open", 3, "game_id", id, "username", username, "user_token", token);
     http_get_response_t* got = http_get(url);
-    int code = p_checkgood(got);
+    gj_keydata_t code = p_checkgood(got);
     http_get_free(got);
     free_url(url);
-    return code;
+    return code.status;
 }
 
 int gj_close_session()
 {
     char* url = p_makeurl("sessions/close", 3, "game_id", id, "username", username, "user_token", token);
     http_get_response_t* got = http_get(url);
-    int code = p_checkgood(got);
+    gj_keydata_t code = p_checkgood(got);
     http_get_free(got);
     free_url(url);
-    return code;
+    return code.status;
 }
 
 int gj_ping_session()
 {
     char* url = p_makeurl("sessions/ping", 3, "game_id", id, "username", username, "user_token", token, "status", "active");
     http_get_response_t* got = http_get(url);
-    int code = p_checkgood(got);
+    gj_keydata_t code = p_checkgood(got);
     http_get_free(got);
     free_url(url);
-    return code;
+    return code.status;
 }
 
 int gj_check_session()
 {
+    return 1;
+}
+
+int gj_set_data(const char* k, gj_type_e type, ...)
+{
+    char datas[128];
+
+    va_list args;
+    va_start(args, type);
+
+    int tint;
+    bool tbool;
+    double tfloat;
+
+    switch(type)
+    {
+    case STRING:
+        strcpy(datas, va_arg(args, char*));
+        break;
+    case INT:
+        sprintf(datas, "%d", va_arg(args, int));
+        break;
+    case BOOL:
+        strcpy(datas, (va_arg(args, int) == 1) ? "true" : "false");
+        break;
+    case FLOAT:
+        sprintf(datas, "%f", va_arg(args, double));
+        break;
+    }
+
+    char* url = p_makeurl("data-store/set", 3, "game_id", id, "key", k, "data", datas);
+    http_get_response_t* got = http_get(url);
+    gj_keydata_t code = p_checkgood(got);
+    http_get_free(got);
+    free_url(url);
+
+    va_end(args);
+
+    return code.status;
+}
+
+int gj_update_data(const char* k, gj_operation_e op, gj_type_e type, ...)
+{
+    char datas[128];
+
+    va_list args;
+    va_start(args, type);
+
+    switch(type)
+    {
+    case STRING:
+        strcpy(datas, va_arg(args, char*));
+        break;
+    case INT:
+        sprintf(datas, "%d", va_arg(args, int));
+        break;
+    case BOOL:
+        printf("BOOLEAN IS NOT A VALID UPDATE TYPE\n");
+        return 0;
+        break;
+    case FLOAT:
+        sprintf(datas, "%f", va_arg(args, double));
+        break;
+    }
+
+    char* url = p_makeurl("data-store/update", 4, "game_id", id, "key", k, "operation", gj_opstrings[op], "value", datas);
+    http_get_response_t* got = http_get(url);
+    gj_keydata_t code = p_checkgood(got);
+    http_get_free(got);
+    free_url(url);
+
+    va_end(args);
+
+    return code.status;
+}
+
+int gj_remove_data(const char* k)
+{
+    char* url = p_makeurl("data-store/remove", 2, "game_id", id, "key", k);
+    http_get_response_t* got = http_get(url);
+    gj_keydata_t code = p_checkgood(got);
+    http_get_free(got);
+    free_url(url);
+
+    return code.status;
+}
+
+gj_data_u gj_get_data(const char* k, gj_type_e type)
+{
+    char* url = p_makeurl("data-store/fetch", 2, "game_id", id, "key", k);
+    http_get_response_t* got = http_get(url);
+    gj_keydata_t code = p_checkgood(got);
+    http_get_free(got);
+    free_url(url);
+
+    gj_data_u data;
+
+    if(code.status == 0) return data;
+
+    int iddata = p_data_get_index(code, "data");
+
+    switch(type)
+    {
+    case STRING:
+        data.s = malloc(strlen(code.values[iddata]));
+        strcpy(data.s, code.values[iddata]);
+        break;
+    case INT:
+        data.i = atoi(code.values[iddata]);
+        break;
+    case BOOL:
+        data.b = (strcmp(code.values[iddata], "true") == 0) ? true : false;
+        break;
+    case FLOAT:
+        data.f = atof(code.values[iddata]);
+        break;
+    }
+
+    return data;
+}
+
+gj_time_t gj_get_time()
+{
+    char* url = p_makeurl("time", 1, "game_id", id);
+    http_get_response_t* got = http_get(url);
+    gj_keydata_t code = p_checkgood(got);
+    http_get_free(got);
+    free_url(url);
+
+    gj_time_t timedata;
+
+    if(!code.status) return timedata;
+
+    strcpy(timedata.timezone, code.values[2]);
+
+    timedata.timestamp = atoi(code.values[1]);
+    timedata.year = atoi(code.values[3]);
+    timedata.month = atoi(code.values[4]);
+    timedata.day = atoi(code.values[5]);
+    timedata.hour = atoi(code.values[6]);
+    timedata.minute = atoi(code.values[7]);
+    timedata.second = atoi(code.values[8]);
+
+    return timedata;
 }
 
 void set_id(char* idy)
--- gj.h
+++ gj.h
@@ -1,9 +1,9 @@
 /*
-    (c) 2021 8_BIT-DEV Under MIT License
+    2021 8_BIT-DEV Under MIT License
     p_ : meaning private function though theres nothing stopping you from using p_ functions
     _e : for enums
     _u : for unions
-    _t : for structs
+    _t : for structs or typedefs
 */
 
 #ifndef GJAPI_H
@@ -15,8 +15,16 @@
 #define free_hash(hash) free(hash)
 #define free_url(url) free(url)
 
+// In the future I plan to add to more extras
+#if defined(GJ_NO_EXTRAS) && defined(GJ_MORE_EXTRAS)
+#error CONFLICTING CONFIGURATION
+#endif // defined
+
 enum gj_type {STRING, INT, BOOL, FLOAT};\
 typedef enum gj_type gj_type_e;
+
+enum gj_operation {APPEND, PREPEND, ADD, SUBTRACT, MULTIPLY, DIVIDE};\
+typedef enum gj_operation gj_operation_e;
 
 union gj_data
 {
@@ -27,20 +35,78 @@ union gj_data
 };
 typedef union gj_data gj_data_u;
 
+struct gj_keydata
+{
+    char names[64][64];
+    char values[64][64];
+    int count;
+    int status;
+};
+typedef struct gj_keydata gj_keydata_t;
+
+struct gj_userdata
+{
+    int id;
+    char type[12];
+    char username[64];
+    char avatar[96];
+    char signed_up[12];
+    int signed_up_timestamp;
+    char last_logged_in[12];
+    int last_logged_in_timestamp;
+    char status[16];
+    char dev_name[64];
+    char dev_website[96];
+    char dev_desc[184];
+};
+typedef struct gj_userdata gj_userdata_t;
+
+struct gj_time
+{
+    int timestamp;
+    char timezone[32];
+    int year;
+    int month;
+    int day;
+    int hour;
+    int minute;
+    int second;
+};
+typedef struct gj_time gj_time_t;
+
+struct gj_trophie
+{
+    int id;
+    char title[32];
+    char desc[128];
+    char diff[12];
+    char image[96];
+    char given[24];
+};
+typedef struct gj_trophie gj_trophy_t;
+
+typedef int gj_rank_t;
+
 void set_id(char* id);
 void set_pkey(char* pkey);
 
 // General functions
 void gj_api();
+int gj_error();
 int gj_login(const char* username, const char* token);
+gj_userdata_t gj_get_user(const char* username, int uid);
+void gj_free_user(gj_userdata_t user);
 
 // trophies
-int gj_give_trophie(int tid);
-int gj_revoke_trophie(int tid);
+gj_trophy_t gj_get_trophy(int tid);
+int gj_give_trophy(int tid);
+int gj_revoke_trophy(int tid);
 
 // scores
 int gj_add_score(int tid, int score);
+int gj_add_score_extra(int tid, int score, const char* data);
 int gj_add_guest_score(const char* name, int tid, int score);
+gj_rank_t gj_get_rank(int tid, int score);
 // shouldnt need to implement more of the score functions
 // theses should do
 
@@ -53,17 +119,21 @@ int gj_check_session(); // not implemented yet
 // data
 // pass everything as a pointer to the set data function
 int gj_set_data(const char* k, gj_type_e type, ...);
+int gj_update_data(const char* k, gj_operation_e op, gj_type_e type, ...);
 int gj_remove_data(const char* k);
-gj_data_u gj_get_data(const char* k);
+gj_data_u gj_get_data(const char* k, gj_type_e type);
 // i'll implement these later
 
 // time
-long gj_get_time();
+gj_time_t gj_get_time();
 // i'll implement this later
 
 // private functions
 char* p_gethash(char* in);
 char* p_makeurl(char* endpoint, int argcount, ...);
-int p_checkgood(http_get_response_t* got);
+gj_keydata_t p_checkgood(http_get_response_t* got);
+
+gj_keydata_t p_parse_reponse(char* repo);
+int p_data_get_index(gj_keydata_t data, const char* name);
 
 #endif // GJAPI_H
